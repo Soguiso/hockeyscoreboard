7c0b7f1fafb102bdafff320ba85e95692db265f9
Soguiso <soguiso@gmail.com>
2021-09-02 15:33:12 -0300

    Atualização de tempo fechada

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,29 +31,25 @@ namespace hockeyScoreboard
         {
             this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timeOption = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 108);
+            this.button1.Location = new System.Drawing.Point(331, 97);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(166, 42);
+            this.button1.Size = new System.Drawing.Size(43, 42);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Iniciar";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(252, 81);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(166, 20);
-            this.textBox1.TabIndex = 1;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // timer1
             // 
@@ -61,34 +57,90 @@ namespace hockeyScoreboard
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(252, 108);
+            this.button2.Location = new System.Drawing.Point(380, 97);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(166, 42);
+            this.button2.Size = new System.Drawing.Size(42, 42);
             this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
+            this.button2.Text = "Parar";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(476, 108);
+            this.button3.Location = new System.Drawing.Point(428, 97);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(166, 42);
+            this.button3.Size = new System.Drawing.Size(45, 42);
             this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
+            this.button3.Text = "Reset";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(382, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tempo";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(340, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(42, 44);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timeOption
+            // 
+            this.timeOption.CausesValidation = false;
+            this.timeOption.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.timeOption.FormattingEnabled = true;
+            this.timeOption.Items.AddRange(new object[] {
+            "15",
+            "20"});
+            this.timeOption.Location = new System.Drawing.Point(340, 70);
+            this.timeOption.Name = "timeOption";
+            this.timeOption.Size = new System.Drawing.Size(121, 21);
+            this.timeOption.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(419, 20);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(42, 44);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(387, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 37);
+            this.label3.TabIndex = 9;
+            this.label3.Text = ":";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(816, 533);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.timeOption);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -98,10 +150,14 @@ namespace hockeyScoreboard
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox timeOption;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,39 +7,84 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
 
 namespace hockeyScoreboard
 {
     public partial class Form1 : Form
     {
+        private DateTime dtInicial;
+        private TimeSpan Calc;
+        private StreamWriter sw;
+        
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void escreveTempoNoArquivo(String tempo)
+        {
+            try
+            {
+                sw = new StreamWriter("time.txt", false, Encoding.ASCII);
+                sw.Write(tempo);
+                sw.Close();
+            }
+            catch (Exception c)
+            {
+                Console.WriteLine("exeption:   " + c.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finaly Block!!!");
+            }
+        }
+
         private void timer1_Tick(object sender, EventArgs e)
         {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
+            //apagaTempoNoArquivo();
+            Calc = dtInicial - DateTime.Now;
+            textBox1.Text = string.Format("{0:ss}", Calc.Minutes.ToString());
+            textBox2.Text = string.Format("{0:ss}", Calc.Seconds.ToString());
+            escreveTempoNoArquivo(textBox1.Text + ":" + textBox2.Text);
             
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
-
+            textBox1.Text = timeOption.Text;
+            if (textBox1.Text == "15")
+            {
+                dtInicial = DateTime.Now.AddMinutes(Convert.ToInt32(textBox1.Text));
+                timer1.Enabled = true;
+            }
+            if (textBox1.Text == "20")
+            {
+                dtInicial = DateTime.Now.AddMinutes(Convert.ToInt32(textBox1.Text));
+                timer1.Enabled = true;
+            }
+            else
+            {
+                timer1.Start();
+            }
+            //stopWatch.Start();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            timer1.Stop();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            timer1.Enabled = false;
+            textBox1.Text = "";
+            textBox2.Text = "";
+            escreveTempoNoArquivo("");
         }
+
+        
     }
 }
